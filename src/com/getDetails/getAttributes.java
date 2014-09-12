c61de4cc73f163b1b84f2e4a675e76573dea38ec
pulapart <sairampulaparthi@gmail.com>
2014-09-12 17:23:20 +0530

    12-sept-14
    12-sept-14

--- src/com/getDetails/getAttributes.java
+++ src/com/getDetails/getAttributes.java
@@ -1,11 +1,14 @@
 package com.getDetails;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Random;
-
+import java.util.TreeMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import com.Utils.sendHttpGet;
 
 
@@ -15,27 +18,20 @@ public class getAttributes {
 	
 public static void main(String[] args) {
 	
-
-	String merchid = "A1Z14AUB2ZPJ4R";		
-	String mpid = "JP" , titlekeywords = "", genkeywords ="";
-	int size = 40;
+//A10XUP92X2DR79
+	String merchid = "AH0MUUSMNZ1IF";		
+	String mpid = "UK" ;// titlekeywords = "", genkeywords ="";
+	//int size = 600;
 		
-	try {
-		String[] sample = fetchSampleasins(merchid, mpid, titlekeywords, genkeywords,  size);
-	
-		if (sample == null) System.out.println("is null");
+	try {		
+		String[] top10 = fetchSampleasins(merchid, mpid, "", "", "", "1", 200);
 		
-		for (int i = 0; i < sample.length; i++) {
-			
-			System.out.println(sample[i]);
-					}
+		String[][] tp = fetchUrls(top10, "UK");
 		
-		String[][] urls =  fetchUrls(sample, mpid);
-		
-		for (int i = 0; i < (urls.length); i++) {
-			System.out.println(i +" ---->>> "+urls[i][0]);
+		for (int i = 0; i < tp.length; i++) {
+			System.out.println(tp[i][1]);
 		}
-			
+					
 	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -44,12 +40,14 @@ public static void main(String[] args) {
 	
 }	
 	
+
+
+public static String[] topNBrands(String merchid, String mpid , int n) throws Exception {
 	
-public static String[] fetchSampleasins(String merchid, String mpid, String titlekeywords, String genkeywords ,int size) throws Exception {
-		
+	
 	try {
-		String qmpid = "", lang="", site = "";
-	    
+		String qmpid = "", lang="", site = ""; 
+		
 	    if (mpid.equals("US")) { lang = "en_US"; site = "www.amazon.com"; qmpid = "ATVPDKIKX0DER"; } 
 	    else if (mpid.equals("UK")) { lang = "en_GB" ; site = "www.amazon.co.uk" ; qmpid = "A1F83G8C2ARO7P" ;}
 	    else if (mpid.equals("CA")) { lang = "en_CA" ; site = "www.amazon.ca" ;  qmpid = "A2EUQ1WTGCTBG2";}
@@ -58,14 +56,112 @@ public static String[] fetchSampleasins(String merchid, String mpid, String titl
 	    else if (mpid.equals("ES")) { lang = "es_ES" ; site = "www.amazon.es" ;  qmpid = "A1RKKUPIHCS9HS";}
 	    else if (mpid.equals("JP")) { lang = "ja_JP" ; site = "www.amazon.co.jp" ;  qmpid = "A1VC38T7YXB528";}
 	    else if (mpid.equals("IT")) { lang = "it_IT" ; site = "www.amazon.it" ;  qmpid = "APJ6JRA9NG5V4";}
-	    int x = randInt(0, 10);
+	        
+	    String getasinquery = "http://"+site+"/gp/internal/search/rsas.html?api=refinementpage&query-searchAlias=&query-node=&query-backbone-node=&query-keywords=&query-merchant-id="
+	    		+ merchid + "&release-ids=&query-marketplace-id="+qmpid+"&query-marketplace-id-override=&refinement=brandtextbin&refinementValuesStartWith=&size=&language="+lang+ "&products_check=1"
+	    				+ "&query-field-1=&query-value-1=&query-field-2=&query-value-2=&query-field-3=&query-value-3=&query-field-4=&query-value-4=&weblab-overrides=&search=Search%21";
 	    
+	    StringBuilder brandstr = new StringBuilder();
+	 	brandstr.append("");
+	 	StringBuilder countstr = new StringBuilder();
+	 	countstr.append("");
+	 	String brands = sendHttpGet.sendGet(getasinquery);
+	 	Document brandsdoc = Jsoup.parse(brands);
+	 	Elements table = brandsdoc.select("table"); 	
+	 	Element tb = table.get(6);
+	 	Element tb2 = tb.select("table").get(2);
+	 	Elements tb3 = tb2.select("tr");
+	 	Elements row = tb3.select("tr");
+	 	
+	 	TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+	 	
+
+	 	
+	 	Integer count = 0 ;
+	 	String origcnt = ""; String fulbrand = ""; String temp = "";
+	 	for (int i = 1; i < (row.size()-1); i++) {
+	 		count = Integer.parseInt(row.get(i).select("td").get(2).text());
+	 		
+	 		origcnt = row.get(i).select("td").get(2).text();
+	 		fulbrand = row.get(i).select("td").get(1).text() + "\t" + origcnt ;
+	 		
+	 		if (map.containsKey(count)) {
+	 			
+	 			temp = map.get(count) + "\t\t" + fulbrand ; 
+	 			
+	 			map.remove(count);
+	 			map.put(count, temp);			
+	 		
+	 		} else 	map.put(count,fulbrand);
+	 	
+	 	}
+	 
+	 	//Sorts brands descending order
+	 	TreeMap<Integer, String> revmap = new TreeMap<Integer, String>(Collections.reverseOrder());
+	 	revmap.putAll(map);
+	 	String[] topbrands = new String[n];
+	 	int c = 0 ;
+	 	for (Map.Entry<Integer, String> entry : revmap.entrySet()) {
+			
+	 		if (c == n) break ;
+	 		
+	 		topbrands[c] = (entry.getValue()); 
+	 
+	 		c++;		
+		}
+	 	
+	 	String[] finalbrands = new String[n];
+	 	
+	 	int fincnt = 0 ;
+	 	
+	 OUT:	for (int i = 0; i < topbrands.length; i++) {
+	 		if (topbrands[i] == null) break OUT;
+	 		
+	 		String[] t1 = topbrands[i].split("\t\t");	
+	 		for (int j = 0; j < t1.length; j++) { 			
+	 		if (fincnt == n) break OUT;	
+	 			finalbrands[fincnt] = t1[j];
+	 			fincnt++;
+			} 		
+		}	 	 	
+ 		return finalbrands;	  	
+	}
+	catch (Exception e) {
+		
+		e.printStackTrace();
+
+		return null;	
+	}
+	
+}
+
+public static String[] fetchSampleasins(String merchid, String mpid, String titlekeywords, String genkeywords, String brand, String page , int size) throws Exception {
+	
+	
+	System.out.println("Merchant ID input : => " + merchid  + " the marketplace input : " + mpid);
+	
+		
+	if (size > 600 ) size = 600 ;
+		
+	try {
+		String qmpid = "", lang="", site = ""; 
+		
+	    if (mpid.equals("US")) { lang = "en_US"; site = "www.amazon.com"; qmpid = "ATVPDKIKX0DER"; } 
+	    else if (mpid.equals("UK")) { lang = "en_GB" ; site = "www.amazon.co.uk" ; qmpid = "A1F83G8C2ARO7P" ;}
+	    else if (mpid.equals("CA")) { lang = "en_CA" ; site = "www.amazon.ca" ;  qmpid = "A2EUQ1WTGCTBG2";}
+	    else if (mpid.equals("DE")) { lang = "de_DE" ; site = "www.amazon.de" ;  qmpid = "A1PA6795UKMFR9";}
+	    else if (mpid.equals("FR")) { lang = "fr_FR" ; site = "www.amazon.fr";  qmpid = "A13V1IB3VIYZZH";}
+	    else if (mpid.equals("ES")) { lang = "es_ES" ; site = "www.amazon.es" ;  qmpid = "A1RKKUPIHCS9HS";}
+	    else if (mpid.equals("JP")) { lang = "ja_JP" ; site = "www.amazon.co.jp" ;  qmpid = "A1VC38T7YXB528";}
+	    else if (mpid.equals("IT")) { lang = "it_IT" ; site = "www.amazon.it" ;  qmpid = "APJ6JRA9NG5V4";}
 	    
-	    String sort = String.valueOf(x) ;
+	    //int x = randInt(0, 10);
+	    //popularity-rank
+	    String sort = "" ;
 	        
 	    String getasinquery = "http://"+site+"/gp/internal/search/rsas.html?api=basic&query-searchAlias=&query-node=&query-backbone-node=&query-keywords="+genkeywords+"&query-merchant-id="+merchid+
-			"&release-ids=&query-marketplace-id="+qmpid+"&query-marketplace-id-override=&query-store=&page=&size="+size+"&sort="+sort+"&product-flavor=&language="+lang+"&eliminate-variation="+
-			"unspecified&products_check=1&query-field-1=search-alias&query-value-1=merchant-items&query-field-2=field-title&query-value-2="+titlekeywords+"&query-field-3=&query-value-3=&query-field-4="+
+			"&release-ids=&query-marketplace-id="+qmpid+"&query-marketplace-id-override=&query-store=&page="+page+"&size="+size+"&sort="+sort+"&product-flavor=&language="+lang+"&eliminate-variation="+
+			"unspecified&products_check=1&query-field-1=search-alias&query-value-1=merchant-items&query-field-2=field-title&query-value-2="+titlekeywords+"&query-field-3=brandtextbin&query-value-3="+brand+"&query-field-4="+
 			"&query-value-4=&metadataNames=&weblab-overrides=&explicitRefinements=&flag-field-1=&flag-value-1=&flag-field-2=&flag-value-2=&flag-field-3=&flag-value-3=&flag-field-4="+
 			"&flag-value-4=&search=Search!";
 	 
@@ -83,6 +179,8 @@ public static String[] fetchSampleasins(String merchid, String mpid, String titl
 		if ( listofasin.equals("")) return null;
 	 	
 	 	String[] sampleasins = listofasin.split(" ");
+	 	
+	  	
  		return sampleasins;	  	
 	}
 	catch (Exception e) {
@@ -98,7 +196,6 @@ public static String[][] fetchUrls(String[] asins, String mplace) throws Excepti
 	StringBuilder asin = new StringBuilder();
 	
 	for (int i = 0; i < asins.length; i++) {
-		
 		asin.append(asins[i]);
 		asin.append(",");		
 	}
@@ -122,15 +219,11 @@ public static String[][] fetchUrls(String[] asins, String mplace) throws Excepti
 	
 	String query = "http://"+site+"/gp/internal/aggregators/pas-test.html?ASINs="+key+"&facets=Description:Low,Media:Low&requestID=&username=&fakePassword=&selectors=";
 	String[][] attribs = new String[asins.length][2];
- //	String[] imgurls = new String[asins.length];
+	
 		try {
 			String physids = sendHttpGet.sendGet(query);
-			
-			Document imgdoc = Jsoup.parse(physids);
-			
+			Document imgdoc = Jsoup.parse(physids);			
 			Elements title = imgdoc.getElementsByTag("pre");
-		
-		//	String[] titles = new String[asins.length];
 			String[] temp , temp2;
 			String titleion = "";
 			int j = 0;
@@ -148,7 +241,6 @@ public static String[][] fetchUrls(String[] asins, String mplace) throws Excepti
 						if (tempstr.contains("\",          '")) {
 							String[] t2 = tempstr.split("\",          '");
 							tempstr = t2[0].toString();
-						//	System.out.println(tempstr.replace("_x{", "&#x").replace("}", ";"));
 							tempstr = tempstr.replace("_x{", "&#x").replace("}", ";").replace("?", "");
 							
 						}
@@ -157,11 +249,7 @@ public static String[][] fetchUrls(String[] asins, String mplace) throws Excepti
 					} catch (Exception e) {
 						tempstr = "couldn't fetch title";
 					}
-							
-				//	titles[j] = temp2[0];
-					
-					attribs[j][0] = tempstr;
-					
+					attribs[j][0] = tempstr;	
 					j++;
 				}
 			
@@ -173,7 +261,6 @@ public static String[][] fetchUrls(String[] asins, String mplace) throws Excepti
 			for (Element el : img) {
 			String src = el.absUrl("src");
 			if (src != null) {
-			//	imgurls[i] = src ;
 				attribs[i][1] = src;
 				}
 			i++;
--- src/noveltyAudit/popAsins.java
+++ src/noveltyAudit/popAsins.java
@@ -3,8 +3,6 @@ package noveltyAudit;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.getDetails.getAttributes;
-
-
 
 /**
  * Servlet implementation class popAsins
@@ -33,14 +29,31 @@ public class popAsins extends HttpServlet {
 
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	
+			
+			
+			
 			response.setContentType("text/html");
 	        PrintWriter out = response.getWriter();
-	       
+	       	
+	        final HttpSession session = request.getSession();
 	        
-			
-	        HttpSession session = request.getSession();
+	        
+	        
 			String merchid = request.getParameter("merchid");
+			
+
+			String page = request.getParameter("page");					
+			int pg = Integer.parseInt(page);
+			
+			String merchantid = request.getParameter("merchantid");
+				
+			if (merchantid != null) {
+				merchid = merchantid ;
+				pg++;		
+				page = String.valueOf(pg);
+			} 
+			if (merchantid == null ) page = "1" ;
+				
 			merchid = merchid.trim();
 	        String mplace = request.getParameter("mplace");
 	        String sz = request.getParameter("size");
@@ -49,24 +62,35 @@ public class popAsins extends HttpServlet {
 	        String titlekeywords = request.getParameter("titlekeywords"); 	        
 	        titlekeywords = titlekeywords.trim().replace(" ", "_");
 	        
+	        String brand = request.getParameter("brand");
+	        brand = brand.trim().replace(" ", "_");
+	        
 	        String genkeywords = request.getParameter("genkeywords"); 	        
 	        genkeywords = genkeywords.trim().replace(" ", "_");
+	        
+	        session.setAttribute("merchid", merchid);
+	        session.setAttribute("merchantid" , merchantid);
+	        session.setAttribute("mplace", mplace);
+	        session.setAttribute("brand", brand);
+	        session.setAttribute("page", page);
+	        
+	        
 	        
 	        if(merchid.equals("") || mplace.equals("") || sz.equals("") ){
 	        //    throw new ServletException("Mandatory fields can't be null or empty");
 	            response.sendRedirect("noveltyaudit.html");
-	        }
-	        
-	        session.setAttribute("merchid", merchid);
-	        session.setAttribute("mplace", mplace);
+	        } 
+	  
 	        int size = Integer.parseInt(sz);
 	        try {
-	        	       	
-	        	merchid = merchid.trim();
-	        	String[] asins = getAttributes.fetchSampleasins(merchid, mplace, titlekeywords, genkeywords, size);
-	        		
+	        	merchid = merchid.trim();  	
+	        //fetchSampleasins(String merchid, String mpid, String titlekeywords, String genkeywords, String brand, String page , int size) 
+	        	String[] asins = getAttributes.fetchSampleasins(merchid, mplace, titlekeywords, genkeywords, brand,page ,size);
+	        	
 	        	if ( asins == null ) throw new Exception ("no asins");
-	        		
+	       		
+				String[] top10 = getAttributes.topNBrands(merchid, mplace, 10);			    			
+					
 	        	String[][] attribs = getAttributes.fetchUrls(asins, mplace); 
 	        	
 	        	String[] imageurls = new String[attribs.length];
@@ -76,13 +100,52 @@ public class popAsins extends HttpServlet {
 	        		imageurls[i] = attribs[i][1];
 	        		titles[i] = attribs[i][0];
 	        	}
-	        	
-	      
-	        	
-	            out.write("<html><head></head><body>");
+	        
+	        	out.write("<html><head></head><body>");
 	        	 
 	            out.write("<title>Sample Size</title>");
-	       	 
+	            
+	            out.write("<FONT FACE=\"arial\">");
+
+	       
+	            out.write("<br>");
+	            
+	            out.write("<form action=\"popAsins\" method = \"get\" target = \"result\"> " ) ;
+                
+                out.write("<input type=hidden name=\"merchantid\" value="+merchid+">");
+                out.write("<input type=hidden name=\"mplace\" value="+mplace+">");
+                out.write("<input type=hidden name=\"size\" value="+size+">");
+                out.write("<input type=hidden name=\"brand\" value="+brand+">");
+                out.write("<input type=hidden name=\"titlekeywords\" value="+titlekeywords+">");
+                out.write("<input type=hidden name=\"genkeywords\" value="+genkeywords+">");
+                out.write("<input type=hidden name=\"page\" value="+page+">");    		
+                out.write("<br><br> <b> Merchant id is  : " + merchid +" &nbsp;    Marketplace is : "+ mplace +"  &nbsp;    Page number : "+ page +" </b> &nbsp;&nbsp;&nbsp;");
+                out.write("<input type=submit value=\"Get Next !\"></form>");
+
+
+	            out.write("<br>");
+	            
+	            out.write("<br>");
+	            
+	            out.write("<div>");
+	            out.write("<table border = '1' CELLPADDING='0' CELLSPACING='1'   bordercolor='black'>");
+	            
+	            for (int i = 0; i < 10; i++) {
+	            	if (top10[i] == null) break;
+	            String[] brands = top10[i].split("\t");
+	            out.write("<tr>");
+				out.write("<td height='20' width='350' align='center' valign='middle'> <font color='Black'> "+brands[0]+" </td>");
+				out.write("<td height='20' width='150' align='center' valign='middle'> <font color='Black'> "+brands[1]+" <br></td>");
+				out.write("</tr>");
+	            }
+	          
+	            out.write("</table>");
+	            out.write("</div>");
+	       
+	            out.write("<br>");
+	            
+	            out.write("<br>");
+	            
 	            String site = "";
 	           	 
 	           	 
@@ -93,11 +156,13 @@ public class popAsins extends HttpServlet {
 	     	    else if (mplace.equals("FR")) { site = "http://www.amazon.fr"; }
 	     	    else if (mplace.equals("ES")) { site = "http://www.amazon.es" ;  }
 	     	    else if (mplace.equals("JP")) { site = "http://www.amazon.co.jp" ;}
-	     	   else if (mplace.equals("IT")) { site = "http://www.amazon.it" ;}
+	     	    else if (mplace.equals("IT")) { site = "http://www.amazon.it" ;}
 	           
 	           	 String dpurl = "";
-	         
-	           	 
+	           	         	            
+	             
+	           out.write("<div>");
+	        
 	           out.write("<table border = '1' CELLPADDING='0' CELLSPACING='1'   bordercolor='black'>"); 
 	        	
 	           for (int i = 0; i < imageurls.length; i=i+4) {
@@ -111,25 +176,25 @@ public class popAsins extends HttpServlet {
 	        			
 	        		if (imageurls[i] != null) {
 	        				dpurl = site+"/dp/"+asins[i];
-	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 200px; height: auto;' src="+imageurls[i]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i]+"</a><br></td>");
+	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 170px; height: auto;' src="+imageurls[i]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i]+"</a><br></td>");
 	        				
 	        			}
 	        			
 	        			if (imageurls[i+1] != null) {
 	        				 dpurl = site+"/dp/"+asins[i+1];
-	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 200px; height: auto;' src="+imageurls[i+1]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i+1]+"</a><br></td>");
+	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 170px; height: auto;' src="+imageurls[i+1]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i+1]+"</a><br></td>");
 	        				
 	        				}
 	        			
 	        			if (imageurls[i+2] != null) {
 	        				dpurl = site+"/dp/"+asins[i+2];
-	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 200px; height: auto;' src="+imageurls[i+2]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i+2]+"</a></br></td>");
+	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 170px; height: auto;' src="+imageurls[i+2]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i+2]+"</a></br></td>");
 	        				
 	        				}
 	        			
 	        			if (imageurls[i+3] != null) {
 	        				dpurl = site+"/dp/"+asins[i+3]; 					
-	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 200px; height: auto;'  src="+imageurls[i+3]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i+3]+"</a><br></td>");
+	        				out.write("<td height='300' width='450' align='center' valign='middle'> <font color='Black'> <b><img style='max-width: 170px; height: auto;'  src="+imageurls[i+3]+" > </b></font><br><a href="+dpurl+" target='_blank'>"+titles[i+3]+"</a><br></td>");
 	        				
 	        			}
 	        				        				
@@ -137,13 +202,20 @@ public class popAsins extends HttpServlet {
 	         		}
 	         		
 	         		out.write("</table> </Font>");
+
+	         	     
+	                out.write("</div>");
+	                out.write("<br><br>");
+	                
+	                
+	         		out.write("</FONT>");
 	         		out.write("</body></html>");
 	     	        	 
 	    	    
 	        } catch (Exception e) {
 			
 	        	if (e.getMessage().equals("no asins")) {
-	        		
+	        		e.printStackTrace();
 	        		out.write("<html><head></head><body>");
     				out.write("<br>");out.write("<br>");out.write("<br>");
     				out.write("<Center>Couldnt fetch sample size for given input <br> <br>  check if input Merchant Id and Marketplace are correct <br> <br> else try different keywords as input</Center>");
